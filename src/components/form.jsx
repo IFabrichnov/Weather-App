8592d066717a60f8a453a3f9eec9801e004b7738
Ivan <39623946+IFabrichnov@users.noreply.github.com>
2020-02-11 19:03:32 +0300

    Changed Readme.md
     Changed Readme.md

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Form = (props) => {
     return <div>
         <form onSubmit={props.weatherMethod}>
-            <input type="text" name="city" placeholder="Город"/>
-            <button>Показать погоду</button>
+            <input type="text" name="city" placeholder="City"/>
+            <button>Show</button>
         </form>
     </div>
 }
